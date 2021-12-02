a7e7fb4ab4694ef71ce83a66a8b06b9324c03d34
Jonathan-Miksis <jmiksis13@gmail.com>
2021-12-01 22:18:14 -0500

    add access token storage, display profile information on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>{{ profile.name }}</p>
+    <p>{{profile.location}}</p>
+    <p>{{profile.url}}</p>
+    <img v-bind:src="profile.avatar_url" alt="">
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data: function () {
+      return {
+        profile: {},
+      };
+  },
+  created: function () {
+    var githubAccessToken = localStorage.getItem("github_access_token");
+    if (githubAccessToken) {
+      axios
+        .get("/github_profile?github_access_token=" + githubAccessToken)
+        .then((response) => {
+          this.profile = response.data;
+        });
+    }
+  },
+};
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <a href="https://github.com/login/oauth/authorize?client_id=1a75328ffa60d689461c">Sign into GitHub</a>
     <h1>{{ message }}</h1>
     <div v-for="post in posts.articles">
       <p><b>Headline: {{ post.description }}</b></p>
@@ -28,7 +29,14 @@ import axios from 'axios'
       axios.get("/news_headlines").then(response => {
         console.log(response.data);
         this.posts = response.data
-      })
+      });
+      var code = this.$route.query.code;
+      if (code) {
+        axios.get("/auth/github/callback?code=" + code).then((response) => {
+          localStorage.setItem("github_access_token", response.data.access_token);
+          this.$router.push("/about");
+        });
+      };
     },
     methods: {},
   };
